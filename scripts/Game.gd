9da1dc8c27bae5fbcb57b949341963d995ce166a
ChrisYeomans <chrisj.yeomans@gmail.com>
2024-05-17 22:08:06 +0100

    Added a win condition

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -4,9 +4,11 @@ extends Sprite2D
 @onready var player_score = $HUD/PlayerScore
 @onready var cpu_score = $HUD/CPUScore
 @onready var ball_timer = $BallTimer
+@onready var winner_label = $HUD/WinnerLabel
 
 var score := [0, 0] # Player, CPU
-const PADDLE_SPEED: int = 500
+const PADDLE_SPEED := 500
+const WINNING_SCORE := 3
 
 func _on_ball_timer_timeout():
 	ball.new_ball()
@@ -14,10 +16,24 @@ func _on_ball_timer_timeout():
 func _on_score_left_body_entered(_body):
 	score[1] += 1
 	cpu_score.text = str(score[1])
-	ball_timer.start()
+	if !check_game_over(score[1], "Player 2"):
+		ball_timer.start()
 	
 func _on_score_right_body_entered(_body):
 	score[0] += 1
 	player_score.text = str(score[0])
-	ball_timer.start()
+	if !check_game_over(score[0], "Player 1"):
+		ball_timer.start()
 
+func check_game_over(score, player):
+	if score == WINNING_SCORE:
+		end_game(player)
+		return true
+	return false
+		
+func end_game(player):
+	winner_label.text = "%s Wins" % player
+	winner_label.visible = true
+	await get_tree().create_timer(3.0).timeout
+	winner_label.visible = false
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
